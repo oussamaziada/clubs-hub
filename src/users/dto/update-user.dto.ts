80ff7bb3561dd462b50df19f10ead6055760bfb5
oussamaziada <oussama.ziada@insat.ucar.tn>
2024-01-26 16:44:39 +0100

    added participate to event function

---
+++ src/users/dto/update-user.dto.ts
@@ -0,0 +1,15 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateUserDto } from './create-user.dto';
+import { IsOptional, IsString } from 'class-validator';
+
+
+export class UpdateUserDto {
+    
+      @IsOptional()
+      @IsString()
+      firstName: string;
+    
+      @IsOptional()
+      @IsString()
+      lastName: string;
+    }
